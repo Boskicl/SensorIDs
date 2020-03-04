a3f9033775c1b07b337edcaccbeb964d86d8ce07
unknown <Ljuboslavboskic@gmail.com>
2020-03-03 19:09:23 -0800

    Codes up to date

--- ADXL355/adxl355.cpp
+++ ADXL355/adxl355.cpp
@@ -8,3 +8,128 @@ Development environment specifics:
 	MAG3110 Breakout Version 1
 Created by: Ljuboslav Boskic
 ******************************************************************************/
+
+#include <Arduino.h>
+#include "adxl355.h"
+#include <Wire.h>
+#include <math.h>
+
+ADXL355::ADXL355() {
+    // Initial Values
+}
+
+// bool ADXL355::initalizeADXL() {
+//     if (readRegister(WHO_AM_I) != WHO_AM_I_RESPC) { //Could not find ADXL355
+//     // Serial.println("Could not find ADXL355")
+//         return false;
+//     }
+//     else // Successfully initialized
+//     {
+//         reset();
+//         return true;
+//     }
+// }
+
+// uint8_t ADXL355::readRegister(uint8_t address){
+//   uint8_t output;
+
+//   Wire.beginTransmission(ADXL355_I2C_ADDRESS);
+//   Wire.write(address);
+//   Wire.endTransmission();
+
+//   delayMicroseconds(2);
+
+//   Wire.requestFrom(ADXL355_I2C_ADDRESS, 1);
+//   while(Wire.available())
+//   {
+// 	output = Wire.read();
+//   }
+
+//   return output;
+// }
+
+// For all Communication for ADXL355 use this - can set range and other settings
+void ADXL355::writeRegist(uint8_t address, uint8_t value) {
+    Wire.beginTransmission(ADXL355_I2C_ADDRESS);                    // Write to the device address of th ADXL355
+    Wire.write(address);
+    Wire.write(value);
+    Wire.endTransmission();
+}
+
+// Start Communications and set the proper address/values 
+void ADXL355::StartADXL(uint8_t address, uint8_t value, uint8_t address2, uint8_t value2) {
+    writeRegist(address,address);
+    delay(100);
+    writeRegist(address2,value2);
+    delay(100);
+    
+}
+
+// Scalefactor- adjusts depending on using 2G, 4G, or 8G setting
+float ADXL355::Scalefactor(int g) {
+    /******************************************************************************
+    (input)  int g   =   Range setting of 2g,4g,8g, input either 2,4,8
+    (return) scale   =   float of scale factor used to calculate Gs 
+    ******************************************************************************/
+    float scale;
+    if (g==2)
+        scale = 4 / (pow(2,20)-1); //2g
+    if (g==4)
+        scale = 8 / (pow(2,20)-1); //4g
+    if (g==8)
+        scale = 16 / (pow(2,20)-1); //8g
+    return (scale);
+
+
+}
+
+// readACCELL- For getting the value in the x/y/z direction of the Gs applied
+void ADXL355::readACCEL(float* x, float* y, float* z, int G) {
+    /******************************************************************************
+    (input) *x   =   This value is returned, input is just a float and a variable for the x value
+    (input) *y   =   This value is returned, input is just a float and a variable for the y value
+    (input) *z   =   This value is returned, input is just a float and a variable for the z value
+    (input) G    =   int G is the value you give either 2,4,8 for the Scalefactor function to adjust to right 2g,4g,8g setting
+    (output) *x   =   Returns X value in Gs, based on certain setting set by G input/Scalefactor
+    (output) *y   =   Returns Y value in Gs, based on certain setting set by G input/Scalefactor
+    (output) *z   =   Returns Z value in Gs, based on certain setting set by G input/Scalefactor
+    ******************************************************************************/
+
+    // Start readout at address
+    double values[3];
+    Wire.beginTransmission(ADXL355_I2C_ADDRESS);
+    Wire.write(Xdata3);                             // Read first byte, auto increments to read rest
+    Wire.endTransmission();
+    Wire.requestFrom(ADXL355_I2C_ADDRESS,9,true);   // Read 9 bytes, 3 from each axis
+    byte x1, x2, x3;
+    for (int i = 0; i <3; ++i){
+        x3 = Wire.read();
+        x2 = Wire.read();
+        x1 = Wire.read();
+        unsigned long tempV = 0;
+        unsigned long value = 0;
+        value = x3;
+        value <<= 12;
+        tempV = x2;
+        tempV <<= 4;
+        value |= tempV;
+        if (x3 & 0x80){
+            value |= 0xFFF00000;
+        }
+        tempV =x1;
+        tempV >>=4;
+        value |= tempV;
+        // Scalefactor(G);
+        values[i] =  Scalefactor(G) * (long)value;
+        // Read each axis and return value
+        *x = (float) values[0];
+        *y = (float) values[1];
+        *z = (float) values[2];
+    }
+    
+}
+
+// Reset ADXL355
+void ADXL355::reset() {
+    writeRegist(RESET,RESET_ALL);
+}
--- ADXL355/adxl355.h
+++ ADXL355/adxl355.h
@@ -27,6 +27,9 @@ Created by: Ljuboslav Boskic
 Addresses specified from Datasheet:
 	Datasheet: ADXL354/ADXL355 (Rev. A)
 	Analog Devices
+
+Define all ADXL355 Registers from page 31 - Register Map 
+   Define [name]      Address [HEX] 
 ******************************************************************************/
 #define Devid_AD			0x00
 #define Devid_MST			0x01
@@ -36,6 +39,7 @@ Addresses specified from Datasheet:
 #define FIFO_Entries		0x05
 #define Temp2				0x06
 #define Temp1				0x07
+/* Start Data addresses */
 #define Xdata3				0x08
 #define Xdata2				0x09
 #define Xdata1				0x0A
@@ -45,6 +49,7 @@ Addresses specified from Datasheet:
 #define Zdata3				0x0E
 #define Zdata2				0x0F
 #define Zdata1				0x10
+/* End Data addresses */
 #define FIFO_data			0x11
 #define Offset_X_H			0x1E
 #define Offset_X_L			0x1F
@@ -60,21 +65,47 @@ Addresses specified from Datasheet:
 #define FIFO_Samples		0x29
 #define Int_Map				0x2A
 #define Sync				0x2B
+/* Range Define */
 #define Range				0x2C
+/* Power Control for Measurment Mode */
 #define Power_Ctl			0x2D
 #define Self_Test			0x2E
 #define Reset				0x2F
 
 /******************************************************************************
-		ADXL355 Range Definitions
+					ADXL355 Range Definitions
 
 Addresses specified from Datasheet:
 	Datasheet: ADXL354/ADXL355 (Rev. A)
 	Analog Devices
 ******************************************************************************/
-Range_2g					01
-Range_4g					10
-Range_8g					11
+const int Range_2g = 0x01;
+const int Range_4g = 0x02;
+const int Range_8g = 0x03;
+
+
+/******************************************************************************
+                		Parameter Setting                 
+ ----------------------------------------------------------------------------
+                    Temperature Parameters                                   
+           Page 33 of Data Sheet - TEMPERATURE DATA REGISTERS               
+******************************************************************************/
+#define ADXL355_Temp_Bias   (float)1852.0                       /* The nominal intercept is 1852 LSB at 25 Deg C */
+#define ADXL355_Temp_Slope  (float)-9.05                        /* The nominal slope os -9.05 LSB / D C          */
+
+/*                    Accelerometer Parameters                                  */
+/*            Page 36-38 of Data Sheet - TEMPERATURE DATA REGISTERS             */
+#define ADXL355_Range       2                                   /* ADXL range 2,4,8 [h]         */
+#define Act_Value           50                                  /* Activity Threshold value     */
+#define Inact_Value         50                                  /* Inactivity Threshold Value   */
+#define Act_Timer           100                                 /* Activity Timer [ms]          */
+#define Inact_Timer         10                                  /* Inactivity Timer [ms]        */
+
+/* Accelerometer write command */
+#define ADXL355_write       0x0
+
+/* Accelerometer read command */
+#define ADXL355_read        0x1
 
 /******************************************************************************
 		ADXL355 Class
@@ -82,19 +113,18 @@ Range_8g					11
 
 class ADXL355
 {
-public:
-	ADXL355();
+    public:
+    ADXL355();
+    // bool initalizeADXL();
+    void writeRegist(uint8_t address, uint8_t value);
+    void StartADXL(uint8_t address, uint8_t value, uint8_t address2, uint8_t value2);
+    float Scalefactor(int g);
+    void readACCEL(float* x, float* y, float* z,int G);
+    void reset();
 
-	//Public Methods
-	uint8_t readRegister(uint8_t address);
-	void writeRegister(uint8_t address, uint8_t value);
-
-	bool dataready();
-
-	void readAdxl(float* x, float* y, float* z);
-
-	void reset();
-private:
+    private:
 
 };
+
+#endif /*End ADXL355_H */
 
