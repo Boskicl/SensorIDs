43644165bbae465c7ee3b0f760ef84fec9b7ba44
unknown <Ljuboslavboskic@gmail.com>
2020-03-03 19:50:43 -0800

    Fixed file

--- ADXL355/adxl355.cpp
+++ ADXL355/adxl355.cpp
@@ -1,6 +1,5 @@
 /******************************************************************************
-MAG3110.h
-ADXL355 Library - MAG3110 Register Map
+ADXL355 Library - ADXL355 Register Map
 Original Creation Date: 10/10/2019
 This file declares the ADXL355 class and registers
 Development environment specifics:
